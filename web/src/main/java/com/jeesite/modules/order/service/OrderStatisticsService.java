1ec25aefa2bfac992464f584b68cc30cf0f53ade
dujunfeng <2762830436@qq.com>
2019-07-16 17:58:04 +0800

    添加订单统计功能

---
+++ web/src/main/java/com/jeesite/modules/order/service/OrderStatisticsService.java
@@ -0,0 +1,75 @@
+package com.jeesite.modules.order.service;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.jeesite.modules.commom.utils.ApiUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+@Slf4j
+public class OrderStatisticsService {
+    @Value("${api.host}")
+    private String apiHost;
+
+    //加载订单统计表格数据
+    public JSONObject tableData(int countValue, int pageNo, int pageSize){
+        JSONObject result = new JSONObject();
+        JSONArray array = new JSONArray();
+        int total = 0;
+        JSONObject ob = new JSONObject();
+        ob.put("countValue", countValue);
+        ob.put("pageNumber", pageNo);
+        ob.put("pageSize", pageSize);
+        JSONObject jsonObject = getResult(ob);
+        if(jsonObject != null){
+            JSONObject temp = jsonObject.getJSONObject("data");
+            array = temp.getJSONArray("entities");
+            total = temp.getInteger("count");
+        }
+
+        result.put("tableData", array);
+        result.put("total", total);
+
+        return result;
+    }
+
+    //加载订单统计图数据
+    public JSONObject echartsData(int countValue){
+        JSONObject result = new JSONObject();
+        JSONArray array = new JSONArray();
+        result.put("echartsData", new JSONArray());
+        JSONObject ob = new JSONObject();
+        ob.put("countValue", countValue);
+        ob.put("pageSize", 30);
+        JSONObject jsonObject = getResult(ob);
+        if(jsonObject != null){
+            JSONObject temp = jsonObject.getJSONObject("data");
+            array = temp.getJSONArray("entities");
+        }
+        result.put("echartsData", array);
+        return result;
+    }
+
+    //访问trip后台接口公共方法
+    private JSONObject getResult(JSONObject paramter){
+        //String requestUrl = apiHost + "/support/statistics/order/queryOrderCount";
+        String requestUrl = "http://192.168.31.201:7150/support/statistics/order/queryOrderCount";
+        String result = ApiUtils.post(requestUrl, paramter);
+        if(!StringUtils.isEmpty(result)){
+            JSONObject resultObject = JSON.parseObject(result);
+            if(resultObject != null){
+                if("200".equals(resultObject.getString("code"))){
+                    return resultObject;
+
+                }
+            }
+        }
+        return null;
+    }
+
+
+}
---
+++ web/src/main/java/com/jeesite/modules/order/web/OrderStatisticsController.java
@@ -0,0 +1,48 @@
+package com.jeesite.modules.order.web;
+
+import com.alibaba.fastjson.JSONObject;
+import com.jeesite.modules.order.service.OrderStatisticsService;
+import com.jeesite.modules.user.entity.UserStatistics;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(value = "${adminPath}/order")
+public class OrderStatisticsController {
+    //注入订单统计服务
+    @Autowired
+    private OrderStatisticsService orderStatisticsService;
+
+    //跳转统计页面
+    @RequestMapping(value = {"statistics", ""})
+    public String statistics(UserStatistics userStatistics, Model model) {
+        model.addAttribute("userStatistics", userStatistics);
+        return "modules/order/orderStatistics";
+    }
+
+    //统计图数据
+    @RequestMapping(value = "echartsData")
+    @ResponseBody
+    public JSONObject echartsData(HttpServletRequest request) {
+        int countValue = Integer.parseInt(request.getParameter("countValue"));
+        JSONObject object = orderStatisticsService.echartsData(countValue);
+        System.err.println(object);
+        return object;
+    }
+
+    //表格数据
+    @RequestMapping(value = "tableData")
+    @ResponseBody
+    public JSONObject tableData(HttpServletRequest request) {
+        int countValue = Integer.parseInt(request.getParameter("countValue"));
+        int pageNo = Integer.parseInt(request.getParameter("pageNo"));
+        int pageSize = Integer.parseInt(request.getParameter("pageSize"));
+        JSONObject object = orderStatisticsService.tableData(countValue, pageNo, pageSize);
+        return object;
+    }
+}
